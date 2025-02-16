300ec26172e5d782241f8867f3469302890f4821
Amogusissus-fr <alexanderhoang2011@gmail.com>
2025-02-16 09:40:59 +0700

    final update

---
+++ Search/search.js
@@ -0,0 +1,10 @@
+let leaveBtn = document.querySelector(".leaveBtn")
+let submitBtn = document.querySelector(".submitBtn")
+
+leaveBtn.addEventListener("click", function () {
+    window.location.href = "/Homepage/home.html"
+})
+
+submitBtn.addEventListener("click", function () {
+    window.location.href = "/Homepage/home.html"
+})
